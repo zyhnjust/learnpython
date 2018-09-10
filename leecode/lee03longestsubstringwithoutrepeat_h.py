8a136e20c4f8b9d7fa78839fa86e86fd7772e421
zyh <zhangyuehui@suanlue.com>
2018-09-10 22:41:00 +0800

    lee006

--- leecode/lee03longestsubstringwithoutrepeat_h.py
+++ leecode/lee03longestsubstringwithoutrepeat_h.py
@@ -4,6 +4,11 @@
 # @Email   : zyh_njust@hotmail.com
 # @File    : lee03longestsubstringwithoutrepeat_h.py
 # @Software: PyCharm
+# 其实这个题目还是很熟练， 首先这个地方的用法
+#
+# countDict[c] = countDict.get(c, 0) + 1  # if no, return 0
+# countDict[s[start]] -= 1
+# 还有就是是不是python 独有的 如果java 怎么说。已经复杂度分析 
 
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
